b2a4996eb0f886279e62752ee641dbc5ed3acb26
Bhatti Luca <luca.bhatti@soprasteria.com>
2022-05-24 11:29:11 +0200

    chet step 3.10

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from './../product.service';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-description',
@@ -16,3 +16,4 @@ export class ProductDescriptionComponent implements OnInit {
   }
 
 }
+
